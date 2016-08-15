7b25949d7a6c8d091268c964ba0cc40992484500
globeriz <globeriz@users.noreply.github.com>
2016-08-15 23:47:16 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 $.getJSON("assets/english.json", function(data){
   var treasures = data.treasures;
   $(document).ready(function(){
+    treasures.sort(function(a, b) {
+        return a.name - b.name;
+    });
     for (var i in treasures)
     {
+      names.push(treasures[i].name);
       opt = $('<option/>', {value: treasures[i].id, text: treasures[i].name});
       $('#select-item').append(opt);
     }
