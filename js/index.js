d1d38d60cdb0ca4baec8ee60287bf1a3772f286f
globeriz <globeriz@users.noreply.github.com>
2016-08-15 23:28:02 +0800

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$.getJSON("../assets/english.json", function(data){
+  var treasures = data.treasures;
+  $(document).ready(function(){
+    for (treasure in treasures)
+    {
+      opt = $('</option>', {value: treasure.id, text: treasure.name}
+      $('#select-item').append(opt);
+    }
+  });
+});
