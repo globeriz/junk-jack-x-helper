154fdcbe32ff1751729e007c45aab97b89451aa7
globeriz <globeriz@users.noreply.github.com>
2016-08-15 23:38:30 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $.getJSON("assets/english.json", function(data){
   var treasures = data.treasures;
   $(document).ready(function(){
-    for (treasure in treasures)
+    for (var treasure in treasures)
     {
       opt = $('<option/>', {value: treasure.id, text: treasure.name});
       $('#select-item').append(opt);
